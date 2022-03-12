1239617c4e42973a93cde78b3783a91a94995b7e
Indra Rahmanto <indra.anto42@gmail.com>
2022-03-12 22:37:54 +0700

    223 An onSuccess Callback

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+import Router from "next/router";
 import useRequest from "../../hooks/use-request";
 
 const Signup = () => {
@@ -12,12 +12,12 @@ const Signup = () => {
       email,
       password,
     },
+    onSuccess: () => Router.push("/"),
   });
 
   const handleSubmitForm = async (e) => {
     e.preventDefault();
-    
-    await doRequest();
+    doRequest();
   };
 
   return (
