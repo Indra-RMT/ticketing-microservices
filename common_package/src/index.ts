d8c95f3190f4d2b56db5b86d046748723a9d3ab0
Indra Rahmanto <indra.anto42@gmail.com>
2022-03-20 22:38:31 +0700

    Updates

--- common_package/src/index.ts
+++ common_package/src/index.ts
@@ -2,14 +2,12 @@ interface Color {
   red: number;
   blue: number;
   green: number;
-  reds: number;
 }
 
 const color: Color = {
-  red: 10,
+  red: 20,
   blue: 10,
   green: 10,
-  reds: 11,
 };
 
 console.log(color);
