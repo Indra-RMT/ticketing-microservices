02c85f971c325d0c9269bc91762f8fd36e690e2b
Indra Rahmanto <indra.anto42@gmail.com>
2022-04-01 20:53:33 +0700

    262. Quick Auth Update

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,8 +6,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("Missing JWT_KEY environment variable");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("Missing MONGO_URI environment variable");
+  }
+
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (err) {
     console.log(err);
