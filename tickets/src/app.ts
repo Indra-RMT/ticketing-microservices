d3670dcecf1dfb93fc1bea2f804599faf2e8c15f
Indra Rahmanto <indra.anto42@gmail.com>
2022-04-03 06:19:01 +0700

    271. Defining the Ticket Model

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError } from "@irtickets/common";
+import { errorHandler, NotFoundError, currentUser } from "@irtickets/common";
+import { createTicketRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true);
@@ -13,6 +14,8 @@ app.use(
     secure: true,
   })
 );
+app.use(currentUser);
+app.use(createTicketRouter);
 
 app.all("/api/users/*", async () => {
   throw new NotFoundError();
