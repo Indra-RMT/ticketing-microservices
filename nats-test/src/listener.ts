8390c9f9ad39f06bdb211b5f181fdab588e5c0cc
Indra Rahmanto <indra.anto42@gmail.com>
2022-07-10 20:39:19 +0700

    316. extending the listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -15,27 +15,7 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('accouting-service');
-
-  const subscribtion = stan.subscribe(
-    'ticket:created',
-    'queue-group-name',
-    options
-  );
-
-  subscribtion.on('message', (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
@@ -81,5 +61,16 @@ abstract class Listener {
     return typeof data === 'string'
       ? JSON.parse(data)
       : JSON.parse(data.toString('utf-8'));
+  }
+}
+
+class TicketCreatedListener extends Listener {
+  subject = 'ticket:created';
+  queueGroupName = 'payments-service';
+
+  onMessage(data: any, msg: Message) {
+    console.log('Event data!', data);
+
+    msg.ack();
   }
 }
