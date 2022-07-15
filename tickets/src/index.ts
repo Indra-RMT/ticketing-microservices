79b059f47faa91932347aa8a4835134870e63624
Indra Rahmanto <indra.anto42@gmail.com>
2022-07-16 02:46:17 +0700

    singleton implementation

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,24 +1,29 @@
-import mongoose from "mongoose";
-
-import { app } from "./app";
+import mongoose from 'mongoose';
+import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error("Missing JWT_KEY environment variable");
+    throw new Error('Missing JWT_KEY environment variable');
   }
   if (!process.env.MONGO_URI) {
-    throw new Error("Missing MONGO_URI environment variable");
+    throw new Error('Missing MONGO_URI environment variable');
   }
 
   try {
+    await natsWrapper.connect(
+      'ticketing',
+      'id-randomstring',
+      'http://nats-srv:4222'
+    );
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
   } catch (err) {
     console.log(err);
   }
 
   app.listen(3000, () => {
-    console.log("Listening on port 3000!!!!");
+    console.log('Listening on port 3000!!!!');
   });
 };
 
