f44ba51569e0260f0be62a43668571a4e3d95de0
Indra Rahmanto <indra.anto42@gmail.com>
2022-07-16 16:25:43 +0700

    356. scaffolding a new route handlers

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -1,15 +1,16 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
-import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError, currentUser } from "@irtickets/common";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes/index";
-import { updateTicketRouter } from "./routes/update";
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@irtickets/common';
+
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes/index';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
 
 const app = express();
-app.set("trust proxy", true);
+app.set('trust proxy', true);
 app.use(json());
 app.use(
   cookieSession({
@@ -19,12 +20,12 @@ app.use(
 );
 
 app.use(currentUser);
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
 
-app.all("/api/tickets/*", async () => {
+app.all('/api/orders/*', async () => {
   throw new NotFoundError();
 });
 
